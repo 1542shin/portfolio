b2bf93bb3bdc6ac825c30953672655d0db72ae5a
Shin <1542shin@gmail.com>
2022-07-07 10:53:06 -0400

    edit aos

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import { About } from "./components/About";
 import React from "react";
 import { Projects } from "./components/projects/Projects";
 import { Contact } from "./components/Contact";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init()
 function App() {
   return (
     <React.Fragment>
